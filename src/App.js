7dc69afd609cb814c99d7e8ff82f2f7de6f81140
yasu <yasu@yasus-MacBook-Pro.local>
2023-03-18 17:55:41 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>figalow website for book orginization</p>
+        <img src="https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=%5B1060%2C626%5D&w=2000&h=1333&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2022%2F02%2F11%2Famerican-wirehair-grey-and-white-754832824-2000.jpg" alt="My Image" />
       </header>
     </div>
   );
